f7fef7e73e4da389999e53c03370881faf27b7fc
kossi973 <150068989+kossi973@users.noreply.github.com>
2024-11-13 21:50:19 +0100

    Initial commit

---
+++ backofficeNationSound77/urls.py
@@ -0,0 +1,15 @@
+
+from rest_framework.routers import DefaultRouter
+from .views import StyleViewSet, ArtisteViewSet, CalendrierViewSet, SceneViewSet, ProgrammeViewSet, EventViewSet, CategoriePoiViewSet, PoiViewSet
+
+router = DefaultRouter()
+router.register(r'styles', StyleViewSet)
+router.register(r'artistes', ArtisteViewSet)
+router.register(r'calendriers', CalendrierViewSet)
+router.register(r'scenes', SceneViewSet)
+router.register(r'programmes', ProgrammeViewSet)
+router.register(r'events', EventViewSet)
+router.register(r'categories', CategoriePoiViewSet)
+router.register(r'pois', PoiViewSet)
+
+urlpatterns=router.urls
