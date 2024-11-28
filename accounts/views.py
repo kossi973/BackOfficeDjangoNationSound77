286f748b9d0428743fb5890a8c785ff97223be1b
kossi973 <150068989+kossi973@users.noreply.github.com>
2024-11-28 21:09:17 +0100

    Ajout des règles de validation à l'inscription

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth import get_user_model, login, logout, authenticate
 from django.shortcuts import redirect, render
-
+import re
 
 User= get_user_model()
 
@@ -16,8 +16,26 @@ def signup(request):
         if not username or not password: 
             messages.error(request, 'Nom d\'utilisateur et mot de passe sont requis.') 
             return render(request, 'accounts/signup.html')
-    
+        
+        if len(username) < 6:
+            messages.error(request, 'Le nom d\'utilisateur doit contenir au moins 6 caractères.') 
+            return render(request, 'accounts/signup.html')
+        
+        #Vérifier unicité du nom
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Ce nom d\'utilisateur est déjà pris.')
+            return render(request, 'accounts/signup.html')
+        
+        if len(password) < 10:
+            messages.error(request, 'Le mot de passe doit contenir au moins 10 caractères.') 
+            return render(request, 'accounts/signup.html')
+        
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+            messages.error(request, 'Le mot de passe doit contenir au moins un caractère spécial.')
+            return render(request, 'accounts/signup.html')
+
         user = User.objects.create_user(username=username, password=password)
+
         if user:
             login(request, user)
             return redirect('Home Page')
